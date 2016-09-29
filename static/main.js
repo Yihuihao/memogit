44cbca126d2870b6cf850626a51adf163a2694f8
Yihui <1594715635@qq.com>
2016-09-29 21:59:59 +0800

    进行了文件的引入，requi引入，requireJS模块化，各模块引入的方式是模块文件的路径

---
+++ static/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2016/9/29.
+ */
+
+//使用requireJS进行基本依赖项设置
+
+require.config({
+    paths:{
+        //路径为相对于自身文件的路径，文件类型 .js 省去
+        'jquery':'js/libs/jquery',
+        'template':'js/libs/template',
+        'jquery-ui':'js/libs/jquery-ui/jquery-ui.min',
+        'underscore':'js/libs/underscore',
+        'backbone':'js/libs/backbone'
+    },
+    //依赖项设置
+    shim:{
+        'backbone':['underscore','jquery'],
+        'jquery-ui':['jquery']
+    }
+});
+
+//将各模块暴露给外部文档
+require([],function(){
+
+});
