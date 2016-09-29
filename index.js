9ffdb927e0ca42cf2009c61732107de8c48bb5f0
Yihui <1594715635@qq.com>
2016-09-29 20:42:12 +0800

    进行了服务器入口文件的配置

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 /**
  * Created by Administrator on 2016/9/29.
  */
+const exp = require('express'),
+    app = exp();
+
+app.use(exp.static('static'));
+
+app.listen(3000,()=>{
+    console.log('·şÎñÆ÷¼àÌı¶Ë¿ÚºÅ3000£¬running....');
+});
+
+
